ab6f13c5095aced694e75a8b7a14d1ea31ea8fd4
Atus <90696526+insaatuss@users.noreply.github.com>
2021-11-16 20:54:39 +0700

    implementasi

---
+++ config/style.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="bootstrap-5.0.2/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="icons/font/bootstrap-icons.css">
